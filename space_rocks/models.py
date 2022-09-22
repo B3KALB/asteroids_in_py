0e529da8a823dba6e81a7a8d814b8212df207de9
B3KALB <B3KALB@gmail.com>
2022-09-22 13:28:55 -0500

    Asteroids_getting_there
    need to add a score and way to reset , high scoreboard, ect,.

--- space_rocks/models.py
+++ space_rocks/models.py
@@ -1,4 +1,10 @@
 from pygame.math import Vector2
+from pygame.transform import rotozoom
+
+from utils import get_random_velocity, load_sound, load_sprite, wrap_position
+
+UP = Vector2(0, -1)
+
 
 class GameObject:
     def __init__(self, position, sprite, velocity):
@@ -11,9 +17,78 @@ class GameObject:
         blit_position = self.position - Vector2(self.radius)
         surface.blit(self.sprite, blit_position)
 
-    def move(self):
-        self.position = self.position + self.velocity
+    def move(self, surface):
+        self.position = wrap_position(self.position + self.velocity, surface)
 
     def collides_with(self, other_obj):
         distance = self.position.distance_to(other_obj.position)
         return distance < self.radius + other_obj.radius
+
+
+class Spaceship(GameObject):
+    MANEUVERABILITY = 4.5
+    ACCELERATION = 2
+    BULLET_SPEED = 15
+
+    def __init__(self, position, create_bullet_callback):
+        self.create_bullet_callback = create_bullet_callback
+        self.laser_sound = load_sound("laser")
+        # Make a copy of the original UP vector
+        self.direction = Vector2(UP)
+
+        super().__init__(position, load_sprite("spaceship"), Vector2(0))
+
+    def rotate(self, clockwise=True):
+        sign = 1 if clockwise else -1
+        angle = self.MANEUVERABILITY * sign
+        self.direction.rotate_ip(angle)
+
+    def accelerate(self):
+        # True accelerate
+        # self.velocity += self.direction * self.ACCELERATION
+        self.velocity = self.direction * self.ACCELERATION
+
+    def draw(self, surface):
+        angle = self.direction.angle_to(UP)
+        rotated_surface = rotozoom(self.sprite, angle, 1)        # SHIP_SIZE
+        rotated_surface_size = Vector2(rotated_surface.get_size())
+        blit_position = self.position - rotated_surface_size * 0.5
+        surface.blit(rotated_surface, blit_position)
+
+    def shoot(self):
+        bullet_velocity = self.direction * self.BULLET_SPEED + self.velocity
+        bullet = Bullet(self.position, bullet_velocity)
+        self.create_bullet_callback(bullet)
+        self.laser_sound.play()
+
+
+class Asteroid(GameObject):
+    def __init__(self, position, create_asteroid_callback, size=3):
+        self.create_asteroid_callback = create_asteroid_callback
+        self.size = size
+
+        size_to_scale = {
+            3: 0.5,
+            2: 0.25,
+            1: 0.125,
+        }
+        scale = size_to_scale[size]
+        sprite = rotozoom(load_sprite("asteroid"), 0, scale)
+
+        super().__init__(position, sprite, get_random_velocity(1, 2))
+
+    def split(self):
+        if self.size > 1:
+            for _ in range(2):
+                asteroid = Asteroid(
+                    self.position, self.create_asteroid_callback, self.size - 1
+                )
+                self.create_asteroid_callback(asteroid)
+
+
+class Bullet(GameObject):
+    def __init__(self, position, velocity):
+        super().__init__(position, load_sprite("bullet"), velocity)
+
+    def move(self, surface):
+        self.position = self.position + self.velocity
